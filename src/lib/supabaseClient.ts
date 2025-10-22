6b2e5a761ff4e5f20a7c81625468e9059776888b
angelomagnoBR <angelomagno@gmail.com>
2025-10-21 22:42:47 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+const supabaseUrl = import.meta.env.https://wfxrrywakpimcvjhdvdi.supabase.co as string;
+const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmeHJyeXdha3BpbWN2amhkdmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwODQ5NTAsImV4cCI6MjA3NjY2MDk1MH0.o32GXOgZGzXeyNdWq4GaNNQ6uys0Ta-VGaOONTHXJng as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
