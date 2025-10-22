bbf5512bc85df5894b7d763d253d0dc676b12731
angelomagnoBR <angelomagno@gmail.com>
2025-10-21 22:46:56 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Valores codificados diretamente - MÁ PRÁTICA!
-const supabaseUrl = "https://wfxrrywakpimcvjhdvdi.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmeHJyeXdha3BpbWN2amhkdmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwODQ5NTAsImV4cCI6MjA3NjY2MDk1MH0.o32GXOgZGzXeyNdWq4GaNNQ6uys0Ta-VGaOONTHXJng";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
