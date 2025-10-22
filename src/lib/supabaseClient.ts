8f5dcd9d6ca3d7fffb85829ab8597794106676c3
angelomagnoBR <angelomagno@gmail.com>
2025-10-21 22:45:04 -0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.https://wfxrrywakpimcvjhdvdi.supabase.co as string;
-const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmeHJyeXdha3BpbWN2amhkdmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwODQ5NTAsImV4cCI6MjA3NjY2MDk1MH0.o32GXOgZGzXeyNdWq4GaNNQ6uys0Ta-VGaOONTHXJng as string;
+// Valores codificados diretamente - MÁ PRÁTICA!
+const supabaseUrl = "https://wfxrrywakpimcvjhdvdi.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmeHJyeXdha3BpbWN2amhkdmRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwODQ5NTAsImV4cCI6MjA3NjY2MDk1MH0.o32GXOgZGzXeyNdWq4GaNNQ6uys0Ta-VGaOONTHXJng";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
