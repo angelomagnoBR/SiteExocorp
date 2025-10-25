a6aa1209106c850c8c6ca26d50d1c4dfe45a1a53
angelomagnoBR <angelomagno@gmail.com>
2025-10-25 00:07:45 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Database from "./components/Database";
+import Database from "./pages/Database";
 import Game from "./pages/Game";
 import NotFound from "./pages/NotFound";
 
@@ -29,4 +29,3 @@ const App = () => (
 );
 
 export default App;
-
