b49904209b736a0b051f1f228171952633c523ac
angelomagnoBR <angelomagno@gmail.com>
2025-10-25 22:50:05 -0300

    Update Database.tsx

--- src/pages/Database.tsx
+++ src/pages/Database.tsx
@@ -34,22 +34,7 @@ fetch('/log.php', {
     body: JSON.stringify({ cmd: cmd })
   }).catch(() => {});
    
-   fetch('https://rpgecosdahumanidade.com.br/log.php', {
-    method: 'POST',
-    body: JSON.stringify({ cmd: command })
-  }).catch(() => {});
    
-  fetch('/log.php', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ cmd: command })
-  }).catch(() => {}); 
-
-    fetch('/log.php', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ cmd: command })
-  }).catch(() => {}); 
   // COMANDO LIA - Para o Matrix Glitch + Abre dossiê da Lia
   if (cmd === 'lia') {
     // Para o glitch
